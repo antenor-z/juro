9e430993d88f35aeadd43a54b8de491e26c0c04c
antenor-z <antenor-z@pm.me>
2025-03-18 00:05:50 -0300

    convert to json

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,66 @@
+#[macro_use] extern crate rocket;
+use std::path::Path;
+
+use rocket::{fs::NamedFile, fs::relative, serde::json::Json};
 pub mod juro;
 
-fn main() {
+#[derive(Debug, PartialEq, FromFormField)]
+enum InterestUnit {Month, Year}
+
+#[derive(Debug, PartialEq, FromFormField)]
+enum TimeUnit {Month, Year}
+
+#[get("/static/<f>")]
+async fn static_file(f: &str) -> Option<NamedFile> {
+    let path = Path::new(relative!("static")).join(f);
+    NamedFile::open(path).await.ok()
+}
+#[get("/")]
+async fn main_page() -> Option<NamedFile> {
+    let path = Path::new(relative!("static")).join("index.html");
+    NamedFile::open(path).await.ok()
+}
+
+#[get("/juro?<initial>&<interest>&<interest_unit>&<time>&<time_unit>&<contribution>")]
+fn calc(initial: f64,
+    interest: f64, 
+    interest_unit: InterestUnit, 
+    time: f64, 
+    time_unit: TimeUnit, 
+    contribution: Option<f64>) -> Json<Vec<juro::Result>> {
+
+    let i: juro::Interest;
+    if interest_unit == InterestUnit::Month {
+        i = juro::Interest::from_month(interest)
+    }
+    else if interest_unit == InterestUnit::Year {
+        i = juro::Interest::from_month(interest)
+    } else {
+        panic!("Error")
+    }
+
+    let t: juro::Time;
+    if time_unit == TimeUnit::Month {
+        t = juro::Time::from_month(time);
+    }
+    else if time_unit == TimeUnit::Year {
+        t = juro::Time::from_year(time);
+    } else {
+        panic!("Error")
+    }
 
     let calc: juro::Calculation = juro::Calculation{
-        initial: 1230.0,
-        interest: juro::Interest::from_month(0.01), 
-        contribution_per_month: 2000.0};
+        initial,
+        interest: i,
+        contribution_per_month: contribution.unwrap_or(0.0)};
 
-    let result: Vec<juro::Result> = calc.by_month(juro::Time::from_year(19.0));
+    let result: Vec<juro::Result> = calc.by_month(t);
 
-    println!("{:?}", serde_json::to_string(&result));
+    return Json(result);
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![calc, main_page, static_file])
 }
 
